b323fdecbce53f261293503c21f5473b8d3687ee
yrdn23 <yrdn@post3.ru>
2024-12-04 17:13:34 +0300

    hw01

---
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,46 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        List<Question> questions = new ArrayList<>();
+
+        try (InputStream stream = getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName())) {
+            if (Objects.isNull(stream)) {
+                throw new QuestionReadException("File %s not found".formatted(fileNameProvider.getTestFileName()));
+            } else {
+                var reader = new BufferedReader(new InputStreamReader(stream));
+                var csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
+                        .withSkipLines(1)
+                        .withType(QuestionDto.class)
+                        .withSeparator(';')
+                        .build();
+
+                for (QuestionDto question : csvToBean) {
+                    questions.add(question.toDomainObject());
+                }
+            }
+        } catch (IOException e) {
+            throw new QuestionReadException("Can't read file %s".formatted(fileNameProvider.getTestFileName()), e);
+        }
+        return questions;
+    }
+}
---
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,19 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public void executeTest() {
+        ioService.printLine("");
+        ioService.printFormattedLine("Please answer the questions below%n");
+        questionDao.findAll().forEach(System.out::println);
+    }
+}
