fe9ae186be3b51d511ac8ee7e9adb96cf5f407a1
yrdn23 <yrdn@post3.ru>
2025-02-05 23:53:10 +0300

    hw06-add-converter

--- hw06-jpa/src/main/java/ru/otus/hw/converters/CommentConverter.java
+++ hw06-jpa/src/main/java/ru/otus/hw/converters/CommentConverter.java
@@ -1,22 +1,15 @@
 package ru.otus.hw.converters;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.models.Comment;
 
-@RequiredArgsConstructor
 @Component
 public class CommentConverter {
 
     public String commentToString(Comment comment) {
-        // TODO
-        /*
-        return "Id: %d, title: %s, author: {%s}, genres: [%s]".formatted(
-                book.getId(),
-                book.getTitle(),
-                authorConverter.authorToString(book.getAuthor()),
-                genreConverter.genreToString(book.getGenre()));
-         */
-        return null;
+        return "book: %s, id: %d, text: %s".formatted(
+                comment.getBook().getTitle(),
+                comment.getId(),
+                comment.getText());
     }
 }
