045c3805c4489ed2629e1c534494aede4667370b
yrdn23 <yrdn@post3.ru>
2025-02-05 23:46:39 +0300

    hw06-add-commands

---
+++ hw06-jpa/src/main/java/ru/otus/hw/converters/CommentConverter.java
@@ -0,0 +1,22 @@
+package ru.otus.hw.converters;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.models.Comment;
+
+@RequiredArgsConstructor
+@Component
+public class CommentConverter {
+
+    public String commentToString(Comment comment) {
+        // TODO
+        /*
+        return "Id: %d, title: %s, author: {%s}, genres: [%s]".formatted(
+                book.getId(),
+                book.getTitle(),
+                authorConverter.authorToString(book.getAuthor()),
+                genreConverter.genreToString(book.getGenre()));
+         */
+        return null;
+    }
+}
