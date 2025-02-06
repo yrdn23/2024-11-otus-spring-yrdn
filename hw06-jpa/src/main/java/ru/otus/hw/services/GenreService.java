8f41a3408c52af06aa59bfdf95111fd9daf8a1b8
yrdn23 <yrdn@post3.ru>
2025-02-06 22:40:05 +0300

    hw06-add-genre-repository

--- hw06-jpa/src/main/java/ru/otus/hw/services/GenreService.java
+++ hw06-jpa/src/main/java/ru/otus/hw/services/GenreService.java
@@ -3,7 +3,10 @@ package ru.otus.hw.services;
 import ru.otus.hw.models.Genre;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GenreService {
     List<Genre> findAll();
+
+    Optional<Genre> findById(long id);
 }
