748d773c351d92c3ffa16a7ac624df04d8315b12
yrdn23 <yrdn@post3.ru>
2025-02-07 00:15:55 +0300

    hw06-add-comment-repository

--- hw06-jpa/src/main/java/ru/otus/hw/repositories/JpaCommentRepository.java
+++ hw06-jpa/src/main/java/ru/otus/hw/repositories/JpaCommentRepository.java
@@ -1,36 +1,36 @@
 package ru.otus.hw.repositories;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Comment;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class JpaCommentRepository implements CommentRepository {
 
-    @Override
-    public Optional<Comment> findById(long id) {
-        // TODO
-        return Optional.empty();
-    }
+    @PersistenceContext
+    private final EntityManager em;
 
     @Override
-    public List<Comment> findByBookId(long bookId) {
-        // TODO
-        return List.of();
+    public Optional<Comment> findById(long id) {
+        return Optional.ofNullable(em.find(Comment.class, id));
     }
 
     @Override
     public Comment save(Comment comment) {
-        //TODO
-        return null;
+        if (comment.getId() == 0) {
+            em.persist(comment);
+            return comment;
+        }
+        return em.merge(comment);
     }
 
     @Override
     public void deleteById(long id) {
-        //TODO
+        findById(id).ifPresent(em::remove);
     }
 }
