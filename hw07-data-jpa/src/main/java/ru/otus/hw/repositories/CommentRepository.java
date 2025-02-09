4efa87d138c34c015d4d9df0c6b76474f01bd515
yrdn23 <yrdn@post3.ru>
2025-02-09 19:51:39 +0300

    hw07-add-template

---
+++ hw07-data-jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -0,0 +1,13 @@
+package ru.otus.hw.repositories;
+
+import ru.otus.hw.models.Comment;
+
+import java.util.Optional;
+
+public interface CommentRepository {
+    Optional<Comment> findById(long id);
+
+    Comment save(Comment comment);
+
+    void deleteById(long id);
+}
