95dd9a1966bb39637515a15ea5453333dc01fed0
yrdn23 <yrdn@post3.ru>
2025-02-10 21:31:59 +0300

    hw07-change-repositories

--- hw07-data-jpa/src/main/java/ru/otus/hw/repositories/GenreRepository.java
+++ hw07-data-jpa/src/main/java/ru/otus/hw/repositories/GenreRepository.java
@@ -1,12 +1,7 @@
 package ru.otus.hw.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.hw.models.Genre;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface GenreRepository {
-    List<Genre> findAll();
-
-    Optional<Genre> findById(long id);
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
