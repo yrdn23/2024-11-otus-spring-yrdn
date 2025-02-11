3d825e21f749fcd62dd09767df704423461b519f
yrdn23 <yrdn@post3.ru>
2025-02-11 19:43:30 +0300

    hw06-add-integration-tests

--- hw06-jpa/src/main/java/ru/otus/hw/repositories/JpaCommentRepository.java
+++ hw06-jpa/src/main/java/ru/otus/hw/repositories/JpaCommentRepository.java
@@ -2,10 +2,12 @@ package ru.otus.hw.repositories;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Comment;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -18,6 +20,14 @@ public class JpaCommentRepository implements CommentRepository {
     @Override
     public Optional<Comment> findById(long id) {
         return Optional.ofNullable(em.find(Comment.class, id));
+    }
+
+    @Override
+    public List<Comment> findByBookId(long bookId) {
+        TypedQuery<Comment> query = em.createQuery(
+                "select c from Comment c where c.book.id = :book_id", Comment.class);
+        query.setParameter("book_id", bookId);
+        return query.getResultList();
     }
 
     @Override
