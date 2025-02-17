cdfc5f3191269e8b4f19844d5f3b0f7db708529b
yrdn23 <yrdn@post3.ru>
2025-02-17 19:14:10 +0300

    hw07-core-review

--- hw07-data-jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw07-data-jpa/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,13 +1,10 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    @Query("select c from Comment c where c.book.id = :bookId")
-    List<Comment> findByBookId(@Param("bookId") long bookId);
+    List<Comment> findByBookId(long bookId);
 }
