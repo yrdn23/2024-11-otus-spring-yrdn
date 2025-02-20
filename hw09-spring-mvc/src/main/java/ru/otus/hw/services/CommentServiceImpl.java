9a8f3139028941d8252341ff14ad122e0903bbee
yrdn23 <yrdn@post3.ru>
2025-02-20 08:33:19 +0300

    hw09-template

---
+++ hw09-spring-mvc/src/main/java/ru/otus/hw/services/CommentServiceImpl.java
@@ -0,0 +1,58 @@
+package ru.otus.hw.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+import ru.otus.hw.models.Comment;
+import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.CommentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+
+    private final BookRepository bookRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Comment> findById(long id) {
+        return commentRepository.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Comment> findByBookId(long bookId) {
+        return commentRepository.findByBookId(bookId);
+    }
+
+    @Transactional
+    @Override
+    public Comment insert(String text, long bookId) {
+        return save(0, text, bookId);
+    }
+
+    @Transactional
+    @Override
+    public Comment update(long id, String text, long bookId) {
+        return save(id, text, bookId);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(long id) {
+        commentRepository.deleteById(id);
+    }
+
+    private Comment save(long id, String text, long bookId) {
+        var book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("Book with id %d not found".formatted(bookId)));
+        var comment = new Comment(id, text, book);
+        return commentRepository.save(comment);
+    }
+}
